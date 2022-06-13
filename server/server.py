b9174a3d0a4bae89010d254a7f921489776dc232
Siddharth Patondikar <83869822+sidpatondikar@users.noreply.github.com>
2022-06-13 12:12:05 +0530

    commit

---
+++ server/server.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 11:08:02 2022
+
+@author: Siddharth Patondikar
+"""
+   
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    sqft_area = int(request.form['sqft_area'])
+    Location = request.form['location']
+    bhk = int(request.form['bhk'])
+    gym = int(request.form['gym'])
+    lift = int(request.form['lift'])
+    parking = int(request.form['parking'])
+    clubhouse = int(request.form['clubhouse'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(Location,sqft_area,bhk,gym,lift,parking,clubhouse)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
